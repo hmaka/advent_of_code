cf84da1952d29e1fb61c061e670786901f28e714
Havish <Havish747@gmail.com>
2025-10-30 23:23:30 -0700

    Day5 pt

--- AdventOfCode2015/src/test/kotlin/Day5.kt
+++ AdventOfCode2015/src/test/kotlin/Day5.kt
@@ -13,11 +13,17 @@ class Day5 {
 
     @Test
     fun pt1(){
-        val ans = input.lines().map { isNiceString(it) }.map { if (it) 1 else 0 }.sum().also { println(it) }
+        val ans = input.lines().map { isNiceStringPt1(it) }.map { if (it) 1 else 0 }.sum().also { println(it) }
         assertEquals(238,ans)
     }
 
-    private fun isNiceString(s: String): Boolean {
+    @Test
+    fun pt2(){
+        val ans = input.lines().map { isNiceStringPt2(it) }.map { if (it) 1 else 0 }.sum().also { println(it) }
+        assertEquals(69,ans)
+    }
+
+    private fun isNiceStringPt1(s: String): Boolean {
         val hasThreeVowels = s.map { if ("aeiou".contains(it)) 1 else 0 }.sum() >= 3
         val hasRepeatedLetter = s.contains(Regex("([a-z])\\1"))
         val doesNotContainBadStrings = listOf("ab", "cd", "pq", "xy").map { !s.contains(it) }.all { it }
@@ -25,5 +31,11 @@ class Day5 {
         return hasThreeVowels && hasRepeatedLetter && doesNotContainBadStrings
     }
 
+    private fun isNiceStringPt2(s: String): Boolean {
+        val hasRepeatedLetterPair = s.contains(Regex("([a-z]{2}).*\\1"))
+        val twoSameLettersWithLetterBetweenThem = s.contains(Regex("([a-z])[a-z]\\1"))
+
+        return hasRepeatedLetterPair && twoSameLettersWithLetterBetweenThem
+    }
 
 }
